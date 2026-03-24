949b03e5f94964dd2436db97d884eb7f2ea3fae8
mktowett <marvintowett@gmail.com>
2026-03-24 16:01:56 +0300

    updated version number

--- telemetry_library/build.gradle.kts
+++ telemetry_library/build.gradle.kts
@@ -125,7 +125,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.NCG-Africa"
                 artifactId = "edge_telemetry_android"
-                version = "2.1.8"
+                version = "2.1.10"
             }
         }
     }
